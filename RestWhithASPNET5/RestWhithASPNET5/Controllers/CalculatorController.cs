85e2e18b77bc8e9f7b85ff8e4d3d708ee0237e61
mateussilva1111 <mateusjose44@hotmail.com>
2022-05-22 16:44:35 -0300

    adicionando operacoes matematicas

---
+++ RestWhithASPNET5/RestWhithASPNET5/Controllers/CalculatorController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWhithASPNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+   
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber)) 
+            {
+                var sum = CovertToNumber(firstNumber) + CovertToNumber(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("invalid Input!!!");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult SubGet(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
+            {
+                var sum = CovertToNumber(firstNumber) - CovertToNumber(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("invalid Input!!!");
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult MultGet(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
+            {
+                var sum = CovertToNumber(firstNumber) * CovertToNumber(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("invalid Input!!!");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult DivGet(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
+            {
+                var sum = CovertToNumber(firstNumber) / CovertToNumber(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("invalid Input!!!");
+        }
+
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber, System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out number);
+            return isNumber;
+        }
+        private decimal CovertToNumber(string strNumber)
+        {
+            decimal decimalvalue;
+
+            if (decimal.TryParse(strNumber, out decimalvalue))
+            {
+                return decimalvalue;
+            }
+
+            return 0;
+        }
+
+        
+    }
+}
