7a4e73d2494c0d4de6c71cc153014998cd774cb5
mateussilva1111 <mateusjose44@hotmail.com>
2022-05-28 14:20:48 -0300

    criando entidade

---
+++ RestWhithASPNET5/RestWhithASPNET5/Controllers/Model/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWhithASPNET5.Controllers.Model
+{
+    public class PersonModel
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
